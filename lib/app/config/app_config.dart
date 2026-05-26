cbe06755435da4d5b840a486350a214d352bcfdc
redrag2105 <akiettran2005@gmail.com>
2026-05-26 23:49:14 +0700

    Emptying branch for full Kodus scan

--- lib/app/config/app_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:latlong2/latlong.dart';
-
-class AppConfig {
-  // Tâm bản đồ mặc định (Gần Đà Nẵng)
-  static const LatLng defaultMapCenter = LatLng(16.042078, 106.206230);
-  static const double defaultMapZoom = 6.0;
-}
